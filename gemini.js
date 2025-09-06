e07f60d6bbb33e86b8f3ce1a2818b10c3faaa833
Bitanu-Ghosh <bitanugkhs@gmail.com>
2025-09-06 12:14:00 +0530

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,4 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const GEMINI_KEY = "AIzaSyB1mqbEt3cFYDT-0l02HLVrOGPIs8F8Dh4";
+const GEMINI_KEY = "Your API KEY";
 export const genAI = new GoogleGenerativeAI(GEMINI_KEY);
+
